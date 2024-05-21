dc9ee8a88871569f30155eaba1732aab5968404c
shirley <shirleylesme04@gmail.com>
2024-05-21 19:07:53 -0400

    first commit

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('importar-pdf/', views.importar_pdf, name='importar_pdf'),
+    path('import-success/', views.import_success, name='import_success'),
+    path('pdf-to-html/', views.pdf_to_html, name='pdf_to_html'),
+    path('carrera-pdf/', views.carrera_pdf_list, name='carrera_pdf_list'),
+     path('imprimir-programas/', views.imprimir_programas, name='imprimir_programas'),
+
+]
